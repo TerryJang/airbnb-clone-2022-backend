5901bdf029c8b475108aa8065f2ba33a466dfa0c
taeil <jty0234@gmail.com>
2023-02-07 23:35:52 +0900

    category model 추가

--- experiences/admin.py
+++ experiences/admin.py
@@ -13,6 +13,10 @@ class ExperienceAdmin(admin.ModelAdmin):
         "created_at",
     )
 
+    list_filter = (
+        "category",
+    )
+
 
 @admin.register(Perk)
 class PerkAdmin(admin.ModelAdmin):
@@ -22,5 +26,6 @@ class PerkAdmin(admin.ModelAdmin):
         "details",
         "explanation",
     )
+
 
 
