910c5f73bb76ce6bd38109eb789ff2a019c31179
taeil <jty0234@gmail.com>
2023-02-10 01:52:30 +0900

    direct messages model 추가

---
+++ direct_messages/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import ChattingRoom, Messages
+
+
+@admin.register(ChattingRoom)
+class ChattingRoomAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "created_at", "updated_at")
+
+    list_filter = ("created_at",)
+
+
+@admin.register(Messages)
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ("text", "user", "room", "created_at")
+    list_filter = ("created_at",)
